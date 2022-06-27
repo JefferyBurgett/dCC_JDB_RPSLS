f537eb3f30bacd7a7b6e81245ddbac58b10e4f81
JefferyBurgett <jeffery.burgett@gmail.com>
2022-06-27 15:25:06 -0500

    Added Run_Game Method To Game Class

--- game.py
+++ game.py
@@ -14,3 +14,12 @@ class Game:
         self.gestures_list = ["Choose 0 for Rock", "Choose 1 for Paper", "Choose 2 for Scissors", "Choose 3 for Lizard", "Choose 4 for Spock"]
         self.player_one = None
         self.player_two = None
+
+#Everything needed to perfom the game       
+    def run_game(self):
+
+        self.display_welcome()
+        self.display_rules()
+        self.player_selection()
+        self.game_mode()
+        self.display_winner()
